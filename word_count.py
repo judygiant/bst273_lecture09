c04c26be2f6bff953103dbcbcbbb06ec1c5579bd
Judy Giant <shutingshen@fas.harvard.edu>
2018-10-04 12:38:29 -0400

    line count

--- word_count.py
+++ word_count.py
@@ -4,16 +4,33 @@ import argparse
 
 parser = argparse.ArgumentParser( description="" )
 
-parser.add_argument(
+"""parser.add_argument(
 	"arg",
 	help="what does this do?",
+)"""
+parser.add_argument(
+"data_file",
+help="path to the file we want to read"
 )
+args=parser.parse_args()
+print (args.data_file)
+fh = open(args.data_file)
+print("the file handle is", fh)
+#-------------------------------------------------------------------------------
+nlines=0
+nwords=0
+nchars=0
+for line in fh:
+	nlines +=1
+	wds= line.strip().split(" ")
+	nwords += len(wds)
+	nchars += len(line)
+
+print ("lines",nlines, "words", nwords, "characters", nchars)
 
 #-------------------------------------------------------------------------------
-# Are there other arguments we need?
-#-------------------------------------------------------------------------------
 
-args = parser.parse_args( )
+
 
 #-------------------------------------------------------------------------------
 # our code for analyzing the data
